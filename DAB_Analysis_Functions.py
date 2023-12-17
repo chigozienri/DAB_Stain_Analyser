dce8da4d9e1c9b3047cbda3bb7f60ec6cda84905
jbeckwith <jsbeckwith@gmail.com>
2023-12-17 18:47:04 +0000

    Update for Gozie

--- DAB_Analysis_Functions.py
+++ DAB_Analysis_Functions.py
@@ -83,7 +83,7 @@ class DAB():
         props = regionprops_table(label_img, properties=('area',
                                                          'axis_minor_length'))
         Area = props['area']
-        indices_toremove = np.unique(np.unique(label_img)[1:]*(Area < 45))[1:]
+        indices_toremove = np.unique(np.unique(label_img)[1:]*(Area < 60))[1:]
         mask=np.isin(label_img,indices_toremove)
         cleaned_mask[mask] = 0
         return cleaned_mask
@@ -162,7 +162,42 @@ class DAB():
         current_params = np.array([LMean, aMean, bMean, thres])
         return image_mask, current_params
     
-    def analyse_DAB_and_cells(self, file, asyn_params=np.array([27, 6, 5, 15]), nuclei_params=np.array([70, 1, -5, 4])):
+    def analyse_DAB(self, file, asyn_params=np.array([27, 6, 5, 15]), use_defaults=1, check_mask=1):
+        """ analyse_DAB function
+        takes file, and uses initial parameters and rate to separate out
+        coloured protein objects
+        then returns table with object information
+        ================INPUTS============= 
+        file is filename
+        asyn_params are initial default Lmean, aMean, bMean and threshold parameters
+        use_defaults is boolean that is 1 (use the default parameters to separate colours) or 0 (get user selection)
+        check_mask outputs a figure that shows the mask---if you don't like it, you can re-run
+        ================OUTPUT============= 
+        asyn_params is parameters used to get particular mask
+        table_asyn is pandas array of asyn data """
+        img = ski.io.imread(file)
+        lab_Image = ski.color.rgb2lab(self.im2double(img))
+        if use_defaults == 0:
+            init_guess = self.get_guess(self, img, lab_Image)
+            asyn_params = np.hstack([init_guess], asyn_params[-1])
+            
+        image_mask_asyn, asyn_params = self.colourFilterLab(lab_Image, asyn_params)
+        image_mask_asyn = self.clean_protein_mask(image_mask_asyn)
+        if check_mask == 1:
+            masks = image_mask_asyn
+            self.plot_masks(img, masks)
+        from skimage.measure import label, regionprops_table
+        label_img_asyn = label(image_mask_asyn)
+        props_asyn = regionprops_table(label_img_asyn, properties=('area',
+                                                         'centroid',
+                                                         'axis_major_length',
+                                                         'axis_minor_length'))
+        import pandas as pd
+        table_asyn = pd.DataFrame(props_asyn)
+        table_asyn['pseudo_circularity'] = self.pseudo_circularity(props_asyn['axis_major_length'], props_asyn['axis_minor_length'])
+        return table_asyn, asyn_params
+    
+    def analyse_DAB_and_cells(self, file, asyn_params=np.array([27, 6, 5, 15]), nuclei_params=np.array([70, 1, -5, 4]), use_defaults=1, check_mask=1):
         """ analyse_DAB_and_cells function
         takes file, and uses initial parameters and rate to separate out
         coloured objects
@@ -173,14 +208,24 @@ class DAB():
         nuclei_params are initial default Lmean, aMean, bMean and threshold parameters
         ================OUTPUT============= 
         table_asyn is pandas array of asyn data
-        table_nuclei is pandas array of nuclei data """
+        table_nuclei is pandas array of nuclei data
+        asyn_params is parameters used to gets asyn mask
+        nuclei_params is parameters used to get nuclear mask """
         img = ski.io.imread(file)
         lab_Image = ski.color.rgb2lab(self.im2double(img))
-        
+        if use_defaults == 0:
+            init_guess = self.get_guess(self, img, lab_Image)
+            asyn_params = np.hstack([init_guess], asyn_params[-1])
+            init_guess_n = self.get_guess(self, img, lab_Image, "select area that is just nuclear staining; press enter when complete")
+            nuclei_params = np.hstack([init_guess_n], nuclei_params[-1])
+            
         image_mask_asyn, asyn_params = self.colourFilterLab(lab_Image, asyn_params)
         image_mask_asyn = self.clean_protein_mask(image_mask_asyn)
         image_mask_nuclei, nucl_params = self.colourFilterLab(lab_Image, nuclei_params, rate=[1,2])
         image_mask_nuclei = self.clean_nuclear_mask(image_mask_nuclei)
+        if check_mask == 1:
+            masks = np.dstack([image_mask_asyn, image_mask_nuclei])
+            self.plot_masks(img, masks)
         from skimage.measure import label, regionprops_table
         label_img_asyn = label(image_mask_asyn)
         props_asyn = regionprops_table(label_img_asyn, properties=('area',
@@ -198,7 +243,7 @@ class DAB():
                                                          'axis_minor_length'))
         table_nuclei = pd.DataFrame(props_nuclei)
         table_nuclei['pseudo_circularity'] = self.pseudo_circularity(props_nuclei['axis_major_length'], props_nuclei['axis_minor_length'])
-        return table_asyn, table_nuclei
+        return table_asyn, table_nuclei, asyn_params, nuclei_params
     
     def get_guess(self, img, lab_image, message="select area that is just DAB-stained protein aggregate; press enter when complete"):
         import cv2
@@ -209,3 +254,26 @@ class DAB():
                               int(r[0]):int(r[0]+r[2])] 
         init_guess = np.mean(np.mean(area, axis=1), axis=0)
         return init_guess
+    
+    def plot_masks(self, img, masks):
+        import matplotlib.pyplot as plt
+        
+        fig, axes = plt.subplots(1, 2, figsize=(8, 4), sharey=True)
+        axes[0].imshow(img)
+        
+        axes[1].imshow(img)
+        if len(masks.shape) > 2: # if multiple masks
+            colors = ['darkred', 'darkblue']
+            for i in np.arange(masks.shape[2]): # plot multiple masks
+                axes[1].contour(masks[:, :, i], [0.5], linewidths=0.5, colors=colors[i])
+        else:
+            axes[1].contour(masks, [0.5], linewidths=0.5, colors='darkred')
+
+        
+        for a in axes:
+            a.axis('off')
+        
+        plt.tight_layout()
+        
+        plt.show()
+        return
